f2718c185658b62ccad7fc5c5ce16b36a6b8dc83
Shekh Muhammad Moinuddin <shekhmuhammad.moinuddin@springfive.com>
2025-04-09 13:27:53 +0530

    fix upload path

--- app.py
+++ app.py
@@ -30,15 +30,15 @@ app = Flask(__name__)
 app.secret_key = "document_generator_secret_key"
 
 
-IS_LOCAL = os.environ.get("VERCEL", "0") != "1"  # Vercel sets VERCEL=1 in prod
+# IS_LOCAL = os.environ.get("VERCEL", "0") != "1"  # Vercel sets VERCEL=1 in prod
 
-if IS_LOCAL:
-    UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "uploads")
-else:
-    UPLOAD_FOLDER = os.path.join("/tmp", "uploads")
+# if IS_LOCAL:
+#     UPLOAD_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "uploads")
+# else:
+#     UPLOAD_FOLDER = os.path.join("/tmp", "uploads")
 
 # Configure upload folder
-#UPLOAD_FOLDER = os.path.join("/tmp", "uploads")
+UPLOAD_FOLDER = os.path.join("/tmp", "uploads")
 OUTPUT_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "output")
 TEMPLATE_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
 ALLOWED_EXCEL_EXTENSIONS = {"xlsx", "xls"}
